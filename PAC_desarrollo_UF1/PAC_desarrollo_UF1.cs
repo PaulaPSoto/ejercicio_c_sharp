66e7a9a3e59ad2752d9e6dc988ba31d998b0f3bd
paulaps <paulaps@DESKTOP-2HAIPHC>
2021-11-22 13:40:43 +0100

    Agregar archivos de proyecto.

---
+++ PAC_desarrollo_UF1/PAC_desarrollo_UF1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PAC_desarrollo_UF1
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+
+            //variables 
+
+            String frase_PAC = "ilerna online programacion 2021";
+            int number_of_letters = 0;
+            int number_of_vowels = 0;
+            char [] vowels_only = new char[11];
+
+          
+
+            //For para recorrer: frase_PAC
+
+            for (int i = 0; i < frase_PAC.Length; i++){
+
+                number_of_letters++;
+
+                if (frase_PAC[i] == 'a' | frase_PAC[i] == 'e' | frase_PAC[i] == 'i' | frase_PAC[i] == 'o' | frase_PAC[i] == 'u'){
+
+                    vowels_only[number_of_vowels] = frase_PAC[i];
+                    number_of_vowels++;
+                }
+            }
+
+            Console.WriteLine("variable: numero total de letras en frase_PAC: " + number_of_letters);
+            Console.WriteLine("variable: numero total de vocales en frase_PAC: " + number_of_vowels);
+            Console.WriteLine("Frase_PAC en mayusculas: " + frase_PAC.ToUpper());
+            Console.WriteLine(vowels_only);
+
+        }
+
+    }
+
+}
